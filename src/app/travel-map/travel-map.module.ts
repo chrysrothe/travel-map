dd5fcd04c337ca0a008bcd96762ee04af074da2a
chrysrothe <rothechristian92@gmail.com>
2020-03-07 17:08:21 +0100

    include google maps and display map

--- src/app/travel-map/travel-map.module.ts
+++ src/app/travel-map/travel-map.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TravelMapRoutingModule } from './travel-map-routing.module';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 import { TravelMapComponent } from './travel-map.component';
 import { MapComponent } from './components/map/map.component';
@@ -10,6 +11,7 @@ import { MapComponent } from './components/map/map.component';
   declarations: [TravelMapComponent, MapComponent],
   imports: [
     CommonModule,
+    GoogleMapsModule,
     TravelMapRoutingModule,
   ]
 })
