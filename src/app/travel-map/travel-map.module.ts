f0b7d2833334d9e4f31ab82746fccc7e0d4ce1ae
chrysrothe <rothechristian92@gmail.com>
2020-03-10 20:42:35 +0100

    refactor sidebar to project footer

--- src/app/travel-map/travel-map.module.ts
+++ src/app/travel-map/travel-map.module.ts
@@ -14,6 +14,7 @@ import { DestinationComponent } from './components/destination/destination.compo
 import { LocationService } from './services/location/location.service';
 import { LightBoxComponent } from './components/light-box/light-box.component';
 import { MapLocationService } from './services/map-location/map-location.service';
+import { SideBarFooterComponent } from './components/side-bar-footer/side-bar-footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { MapLocationService } from './services/map-location/map-location.service
     SideBarComponent,
     DestinationListComponent,
     DestinationComponent,
-    LightBoxComponent
+    LightBoxComponent,
+    SideBarFooterComponent
   ],
   imports: [
     CommonModule,
