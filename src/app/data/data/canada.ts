a34906372e9152b699b8f41060c0ae2bad1f124a
chrysrothe <rothechristian92@gmail.com>
2020-03-27 21:28:19 +0100

    add more canada pictures

--- src/app/data/data/canada.ts
+++ src/app/data/data/canada.ts
@@ -284,5 +284,178 @@ export const canada: Destination = {
             },
             destinatonKey: DestinationEnum.Canada,
         },
+        {
+            name: 'Golden-mantled ground squirrel',
+            picture: '../assets/pictures/canada/lg/golden_mantled_squirrel.jpg',
+            marker: {
+                location: new google.maps.LatLng(51.407809, -116.239474),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/golden_mantled_squirrel_sm.jpg',
+                        scaledSize: {
+                            height: 75,
+                            width: 50,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+            cannotBeTitle: true,
+        },
+        {
+            name: 'Herbert Lake',
+            picture: '../assets/pictures/canada/lg/herbert_lake.jpg',
+            marker: {
+                location: new google.maps.LatLng(51.460720, -116.224009),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/herbert_lake_sm.jpg',
+                        scaledSize: {
+                            height: 50,
+                            width: 75,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+        },
+        {
+            name: 'Bow Lake',
+            picture: '../assets/pictures/canada/lg/bow_lake.jpg',
+            marker: {
+                location: new google.maps.LatLng(51.680528, -116.462656),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/bow_lake_sm.jpg',
+                        scaledSize: {
+                            height: 50,
+                            width: 75,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+        },
+        {
+            name: 'Peyto Lake',
+            picture: '../assets/pictures/canada/lg/peyto_lake.jpg',
+            marker: {
+                location: new google.maps.LatLng(51.717116, -116.506608),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/peyto_lake_sm.jpg',
+                        scaledSize: {
+                            height: 50,
+                            width: 75,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+        },
+        {
+            name: 'Rainbow at Peyto Lake',
+            picture: '../assets/pictures/canada/lg/peyto_lake_rainbow.jpg',
+            marker: {
+                location: new google.maps.LatLng(51.717124, -116.506855),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/peyto_lake_rainbow_sm.jpg',
+                        scaledSize: {
+                            height: 50,
+                            width: 75,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+        },
+        {
+            name: 'Nature',
+            picture: '../assets/pictures/canada/lg/nature.jpg',
+            marker: {
+                location: new google.maps.LatLng(52.110545, -116.972977),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/nature_sm.jpg',
+                        scaledSize: {
+                            height: 75,
+                            width: 50,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+            cannotBeTitle: true,
+        },
+        {
+            name: 'Athabasca Falls',
+            picture: '../assets/pictures/canada/lg/athabasca_falls.jpg',
+            marker: {
+                location: new google.maps.LatLng(52.664288, -117.883880),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/athabasca_falls_sm.jpg',
+                        scaledSize: {
+                            height: 50,
+                            width: 75,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+        },
+        {
+            name: 'Second Bridge, Maligne Canyon',
+            picture: '../assets/pictures/canada/lg/maligne_canyon.jpg',
+            marker: {
+                location: new google.maps.LatLng(52.920271, -118.002812),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/maligne_canyon_sm.jpg',
+                        scaledSize: {
+                            height: 75,
+                            width: 50,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+            cannotBeTitle: true,
+        },
+        {
+            name: 'Moose',
+            picture: '../assets/pictures/canada/lg/moose.jpg',
+            marker: {
+                location: new google.maps.LatLng(52.836095, -117.718311),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/moose_sm.jpg',
+                        scaledSize: {
+                            height: 50,
+                            width: 75,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+        },
+        {
+            name: 'Maligne lake',
+            picture: '../assets/pictures/canada/lg/maligne_lake.jpg',
+            marker: {
+                location: new google.maps.LatLng(52.728371, -117.637530),
+                options: {
+                    icon: {
+                        url: '../assets/pictures/canada/sm/maligne_lake_sm.jpg',
+                        scaledSize: {
+                            height: 50,
+                            width: 75,
+                        },
+                    },
+                },
+            },
+            destinatonKey: DestinationEnum.Canada,
+        },
     ],
 };
